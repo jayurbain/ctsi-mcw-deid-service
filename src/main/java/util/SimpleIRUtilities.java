915510d7b5398cc3ebda8299857658360d76a70a
jayurbain <jay.urbain@gmail.com>
2017-08-01 15:19:06 -0500

    license info, updated instructions

--- src/main/java/util/SimpleIRUtilities.java
+++ src/main/java/util/SimpleIRUtilities.java
@@ -1,22 +1,24 @@
-// Copyright (c) 2010 Jay F. Urbain. All Rights Reserved.
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-//
-// For more information, bug reports, fixes, contact:
-//    Jay Urbain
-//    jay.urbain@gmail.com
+/**
+    This file is part of "CTSI MCW Deidentification" for removing
+    protected health information from medical records.
+
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    "CTSI MCW Deidentification" is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with "CTSI MCW Deidentification."  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @author jayurbain, jay.urbain@gmail.com
+ * 
+ */
 
 package util;
 import java.util.Date;
